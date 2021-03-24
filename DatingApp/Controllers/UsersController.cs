db3cd2c3c291e87c74a454141bd64ad3cc18d6e7
Julio Alberto Cano López <jcanol@rentingcolombia.com>
2021-03-23 22:03:34 -0500

    Adding new controller and turn the code into async

---
+++ DatingApp/Controllers/UsersController.cs
@@ -0,0 +1,44 @@
+﻿using DatingApp.Data;
+using DatingApp.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DatingApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync().ConfigureAwait(false);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id).ConfigureAwait(false);
+        }
+
+        [HttpPost]
+        public async Task Post(AppUser user)
+        {
+            await _context.Users.AddAsync(user).ConfigureAwait(false);
+            await _context.SaveChangesAsync().ConfigureAwait(false);
+        }
+    }
+}
